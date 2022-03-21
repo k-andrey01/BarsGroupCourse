01b711eeceacb10128c4384cce5fb30a5b34e25f
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2022-03-22 02:50:21 +0300

    First study task

---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyPress = new TestClass();
+            keyPress.OnKeyPressed += (Sender, ButtonKey) => Console.WriteLine($"\nYou pressed '{ButtonKey}' button!");
+            keyPress.Run();
+        }
+    }
+}
---
+++ FirstTask/FirstTask/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    internal class TestClass
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            bool cPressed = false;
+            while (!cPressed)
+            {
+                var key = Console.ReadKey();
+                OnKeyPressed?.Invoke(this, key.KeyChar);
+                if ((key.KeyChar.Equals('c'))||(key.KeyChar.Equals('с')))
+                    cPressed= true;
+            }
+            Console.WriteLine("Exit from programm");
+        }
+    }
+}
